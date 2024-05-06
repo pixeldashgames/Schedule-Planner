3c58890a70a7d1e6abe80daf1f66cbc8ea75df76
Nuebe <ernesto.caballero@datys.cu>
2024-05-06 15:24:06 -0400

    Adding restricction.♠

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def load_data_from_json(file_path):
 def main():
     
     # Cargar datos desde el archivo JSON
-    data = load_data_from_json('datos.json')
+    data = load_data_from_json('src\datos.json')
 
     subjects_name_list = data['subjects']
     dict_subjects_by_time = data['subject_times']
@@ -23,8 +23,18 @@ def main():
     days = data['days']
     dict_teachers_to_subjects = data['teachers_to_subjects']
 
+    hards_true = data['hards_true']
+    hards_false = data['hards_false']
+    softs_max = data['softs_max']
+    softs_min = data['softs_min']
+
     solver = PlanificadorHorario(subjects_name_list, dict_subjects_by_time, teachers_names, classrooms_names, groups_names,
                                dict_group_subject_time, shifts, days, dict_teachers_to_subjects)
+
+    for restriction in hards_true: solver.agregar_restricciones_hard_verdaderas(hards_true[restriction]["teachers_names"], hards_true[restriction]["subjects_names"], hards_true[restriction]["classrooms_names"], hards_true[restriction]["groups_names"], hards_true[restriction]["shifts"], hards_true[restriction]["days"])
+    for restriction in hards_false: solver.agregar_restricciones_hard_falsas(hards_false[restriction]["teachers_names"], hards_false[restriction]["subjects_names"], hards_false[restriction]["classrooms_names"], hards_false[restriction]["groups_names"], hards_false[restriction]["shifts"], hards_false[restriction]["days"])
+    for restriction in softs_max: solver.agregar_restricciones_suaves_maximizar(softs_max[restriction]["teachers_names"], softs_max[restriction]["subjects_names"], softs_max[restriction]["classrooms_names"], softs_max[restriction]["groups_names"], softs_max[restriction]["shifts"], softs_max[restriction]["days"], softs_max[restriction]["alpha"])
+    for restriction in softs_min: solver.agregar_restricciones_suaves_minimizar(softs_min[restriction]["teachers_names"], softs_min[restriction]["subjects_names"], softs_min[restriction]["classrooms_names"], softs_min[restriction]["groups_names"], softs_min[restriction]["shifts"], softs_min[restriction]["days"], softs_min[restriction]["alpha"])
 
     df = solver.resolver_horario()
 
